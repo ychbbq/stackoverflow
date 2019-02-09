1829f37a36f32f41a7e81bf700f8972563b6f721
root <ychbbq@gmail.com>
2019-02-09 04:10:49 +0000

    added a temp function

--- Salary_ML.py
+++ Salary_ML.py
@@ -105,6 +105,9 @@ def find_optimal_lm_mod(X, y, cutoffs, test_size = .30, random_state=42, plot=Tr
 
     return r2_scores_test, r2_scores_train, lm_model, X_train, X_test, y_train, y_test
 
+def temp:
+    return 1
+
 def main():
     df = pd.read_csv('../Part1/stackoverflow/survey_results_public.csv')
     X, y = clean_data(df)
